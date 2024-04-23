03b3ac2e9de757f408294ad8bafffc2ef7e65b9c
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2024-04-22 20:28:13 -0500

    upload: primer modulo

---
+++ 6.primer_modulo.js/index.js
@@ -0,0 +1,24 @@
+function add(x,y) {
+    return x + y;
+};
+
+function subtract(x,y) {
+    return x - y;
+};
+
+function multiply(x,y) {
+    return x * y;
+};
+
+function divided(x,y) {
+    return x / y;
+};
+
+module.exports = {
+    add,
+    subtract,
+    divided,
+    multiply
+}
+
+// una mejor manera seria usando export
---
+++ 6.primer_modulo.js/main.js
@@ -0,0 +1,9 @@
+const math = require('./index.js');
+// es comun que al exportar, se guarde en una variable del nombre del modulo
+
+// Uso del Modulo
+console.log(math);
+console.log(math.add(1,2));
+console.log(math.subtract(1,2));
+console.log(math.multiply(1,2));
+console.log(math.divided(1,2));
